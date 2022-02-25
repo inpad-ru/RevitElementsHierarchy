fa595589d686816db82c2e86bf44b45f0e83c454
sagittariusolli <sagittariusolli@gmail.com>
2022-02-25 14:46:59 +0500

    Parameter change enabled

--- RevitElementsHierarchy/Models/Inst.cs
+++ RevitElementsHierarchy/Models/Inst.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.ObjectModel;
+using Autodesk.Revit.DB;
 
 namespace InpadPlugins.RevitElementsHierarchy.Models
 {
     public class Inst
     {
         public string Name { get; set; }
+
+        public Element Element { get; set; }
         public ObservableCollection<Inst> InstItem { get; set; }
         public ObservableCollection<Parameter> Parameters { get; set; }
     }
--- RevitElementsHierarchy/Models/Parameter.cs
+++ RevitElementsHierarchy/Models/Parameter.cs
@@ -3,6 +3,11 @@
     public class Parameter
     {
         public string Name { get; set; }
+
+        public Autodesk.Revit.DB.Parameter RevParameter { get; set; }
         public string Value { get; set; }
+
+        public string OldValue { get; set; }
+
     }
 }
